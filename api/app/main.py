9debb8ca10275b6e771aa05ebcddf579ff302102
Lorenzo Lupetti <lorenzo.lupetti3@gmail.com>
2023-10-23 17:16:13 +0200

    Aggiunto healthcheck sulla api e livenessprobe nel deploy. creata route e funzione che stampa tutti i logs di tutti i pods

--- api/app/main.py
+++ api/app/main.py
@@ -1,12 +1,14 @@
 import sys
-from fastapi import FastAPI, status, HTTPException
+import logging
+from fastapi import FastAPI, status, HTTPException, Request
 from pydantic import BaseModel
 from kubernetes import client, config
 import os
+import re
 import random
 
 # print(os.getenv("SECRET_KEY"))
-
+## ----------------- SETUP ----------------- ##
 # FASTAPI
 app = FastAPI()
 
@@ -16,8 +18,9 @@ try:
 except:
     # load_kube_config throws if there is no config, but does not document what it throws, so I can't rely on any particular type here
     config.load_incluster_config()
-v1 = client.CoreV1Api()
+v1 = client.CoreV1Api()  
 
+## ----------------- MODELS ----------------- ##
 
 class Rank(BaseModel):
     id: int
@@ -26,6 +29,7 @@ class Rank(BaseModel):
     # description: str | None = None
     # tags: list[str] = []
 
+## ----------------- ROUTES ----------------- ##
 
 @app.get("/")
 async def root():
@@ -36,7 +40,7 @@ async def root():
 
     return {"message": "Hello World"}
 
-
+# RANKINGS
 @app.get("/rankings")
 async def rankings() -> list[Rank]:
     return [{"id": 1, "rank": 10}, {"id": 2, "rank": 11}]
@@ -52,21 +56,52 @@ async def create_rank(r: Rank) -> list[Rank]:
     return [{"id": 1, "rank": 10}, {"id": 2, "rank": 11}, r]
 
 
+# K8
 @app.get("/pods")
 async def get_pods() -> list[str]:
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    return [pod.status.pod_ip for pod in ret.items if pod.status.pod_ip is not None]
+    pods = v1.list_namespaced_pod("default",watch=False).items
+    return [pod.status.pod_ip for pod in pods if pod.status.pod_ip is not None]
 
-
-@app.get("/podid")
+@app.get("/pods/logs")
+async def get_pods_logs(request: Request) -> dict[str,str]:
+    # get all pods
+    ret = v1.list_namespaced_pod("default",watch=False)
+    pods_logs = {} # return item
+    for pod in ret.items:  
+            # extract name and logs from the pod
+            name = pod.metadata.name
+            logs = v1.read_namespaced_pod_log(name=name, namespace="default")
+            # if the request specify to include health check i will not filter the logs
+            if request.query_params.get("health") == None or request.query_params.get("health") == "false" :
+                # Split the log string into individual lines
+                log_lines = logs.split('\n')
+                # Filter out log lines that contain the "/health" endpoint
+                filtered_log_lines = [line for line in log_lines if "/health" not in line]
+                # Join the filtered lines to reconstruct the log string
+                logs = '\n'.join(filtered_log_lines)
+                # Split the log string into individual lines
+                log_lines = logs.split('\n')
+                # Filter out log lines that contain the "/health" endpoint
+                filtered_log_lines = [line for line in log_lines if "/health" not in line]
+                # Join the filtered lines to reconstruct the log string
+                logs = '\n'.join(filtered_log_lines)
+            # add the logs to the dictionary
+            pods_logs[name] = logs
+    return pods_logs
+	
+@app.get("/pod/clusterid")
 async def get_current_pod_id() -> str:
-	# python or between env or unknown  
-  return (os.getenv("HOSTNAME") or "unknown")
-	 
+  return (os.getenv("HOSTNAME") or "unknown") 
 
 
+# HEALTH
+@app.get("/health")
+async def health_check() -> str:
+	return "OK"
 
-@app.get("/error")
+@app.get("/badHealth")
 async def throw_error():
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="Item not found")
+    raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail="Service unavailable")
+
+
+# OTHER
