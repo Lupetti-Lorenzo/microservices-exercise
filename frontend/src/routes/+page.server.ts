89206caaf4b79f80272e054befb3dc86a8e54dc2
Lorenzo Lupetti <lorenzo.lupetti3@gmail.com>
2023-10-24 14:42:57 +0200

    Provato traefik, per ora non va

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-	const res = await fetch(`http://${process.env.API_IP || "localhost"}:80`);
+	const res = await fetch(
+		`http://${process.env.API_IP || "localhost"}:${process.env.API_PORT || 80}`
+	);
 	const data = await res.text();
 	return { hello: data };
 }
